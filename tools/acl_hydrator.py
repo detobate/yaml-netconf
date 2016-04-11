b325f7264ac1513b396e4acca53ba3279016757b
fifi <laszlo.fintor@gmail.com>
2016-04-11 19:14:04 +0100

    new file:   objects/net_bogon_cymru.yaml 	new file:   objects/net_broadcast_0.yaml 	new file:   objects/net_broadcast_255.yaml 	new file:   objects/net_rfc1112.yaml 	new file:   objects/net_rfc1122.yaml 	new file:   objects/net_rfc1918.yaml 	new file:   objects/net_rfc2544.yaml 	new file:   objects/net_rfc3068.yaml 	new file:   objects/net_rfc3171.yaml 	new file:   objects/net_rfc3330.yaml 	new file:   objects/net_rfc3927.yaml 	new file:   objects/net_rfc5735.yaml 	new file:   objects/net_rfc5736.yaml 	new file:   objects/net_rfc5737.yaml 	new file:   objects/net_rfc5771.yaml 	new file:   objects/net_rfc6598.yaml 	new file:   objects/net_rfc6890.yaml 	new file:   objects/net_rfc922.yaml 	modified:   tools/acl_hydrator.py

--- tools/acl_hydrator.py
+++ tools/acl_hydrator.py
@@ -1,25 +1,43 @@
 import yaml
 import click
+import ipaddress
 
 
 @click.command()
-@click.option('--file_arg', default='example', help='ACL to process in YAML format with path, default is example.yaml in ./ ')
-def get_file(file_arg):
-    with open("{}".format(file_arg), 'r') as stream:
-        try:
-            print(yaml.load(stream))
-        except yaml.Event as exc:
-            print(exc)
-
-
-def main():
+@click.option('--acl', default='../rulesets/example_ACL.yaml', help='The input ACL with relative path.')
+@click.option('--object_dir', default='../objects/', help='Relative path to objects')
+@click.option('--hydrated_file', default='./hydrated.yaml', help='Output file after hydration')
+def main(acl, object_dir, hydrated_file):
     print_title()
-    get_file()
+    result = load_file(acl)
+    process_objects(result, object_dir)
+
 
 def print_title():
     print('------------------------------')
-    print('--------HYDRATING ACL---------')
+    print('      ACL HYDRATOR APP')
     print('------------------------------')
+
+
+def load_file(file):
+    with open(file, 'r') as stream:
+        try:
+            return yaml.load(stream)
+        except yaml.MarkedYAMLError as exc:
+            print(exc)
+
+
+def process_objects(raw_acl, object_dir):
+    rules = raw_acl['ACL']
+    for rule in rules['rules']:
+        try:
+            if ipaddress.ip_network(rule['source']):
+                print(rule['source'])
+        except ValueError as VE:
+            acl_object = (rule['source'].strip('{{ ').strip(' }}'))
+            print(load_file(object_dir + acl_object + '.yaml'))
+
+    return None
 
 
 if __name__ == '__main__':
