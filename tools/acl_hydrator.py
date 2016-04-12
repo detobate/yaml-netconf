8cf0badfda0454f36c7b7a1aee2240b3622c9984
fifi <laszlo.fintor@gmail.com>
2016-04-12 01:06:55 +0100

    modified:   tools/acl_hydrator.py

--- tools/acl_hydrator.py
+++ tools/acl_hydrator.py
@@ -1,6 +1,5 @@
 import yaml
 import click
-import ipaddress
 
 
 @click.command()
@@ -9,8 +8,9 @@ import ipaddress
 @click.option('--hydrated_file', default='./hydrated.yaml', help='Output file after hydration')
 def main(acl, object_dir, hydrated_file):
     print_title()
-    result = load_file(acl)
-    process_objects(result, object_dir)
+    new_rule_set = load_rulesets(acl, object_dir)
+    print(new_rule_set)
+    # write_rule_set(rule_set, hydrated_file)
 
 
 def print_title():
@@ -19,25 +19,69 @@ def print_title():
     print('------------------------------')
 
 
-def load_file(file):
+def load_rulesets(file, object_dir):
     with open(file, 'r') as stream:
-        try:
-            return yaml.load(stream)
-        except yaml.MarkedYAMLError as exc:
-            print(exc)
+        master_dict = yaml.load(stream)
+        counter = 0
+
+        '''create copy of original and later only replace the bits that get hydrated. All the descriptions etc remain
+        untouched'''
+
+        new_master_dict = master_dict
+
+        for rule in master_dict['ACL']['rules']:
+
+            '''check whether there is a further reference via curly brackets to another object, if so invoke
+            the recursive load_object function which returns with the full list'''
+
+            if rule['source'][0:2] == '{{':
+                object_file_name = rule['source'].strip('{{').strip('}}').strip() + '.yaml'
+                list_of_objects = load_object(object_dir + object_file_name, object_dir)
+                new_master_dict['ACL']['rules'][counter]['source'] = list_of_objects
+
+            ''' do the above for the other four tuples i.e. ports, dest etc'''
+
+            if rule['dest'][0:2] == '{{':
+                object_file_name = rule['dest'].strip('{{').strip('}}').strip() + '.yaml'
+                list_of_objects = load_object(object_dir + object_file_name, object_dir)
+                new_master_dict['ACL']['rules'][counter]['dest'] = list_of_objects
+
+            if rule['sport'][0:2] == '{{':
+                object_file_name = rule['sport'].strip('{{').strip('}}').strip() + '.yaml'
+                list_of_objects = load_object(object_dir + object_file_name, object_dir)
+                new_master_dict['ACL']['rules'][counter]['sport'] = list_of_objects
+
+            if rule['dport'][0:2] == '{{':
+                object_file_name = rule['dport'].strip('{{').strip('}}').strip() + '.yaml'
+                list_of_objects = load_object(object_dir + object_file_name, object_dir)
+                new_master_dict['ACL']['rules'][counter]['dport'] = list_of_objects
+
+            if rule['proto'][0:2] == '{{':
+                object_file_name = rule['proto'].strip('{{').strip('}}').strip() + '.yaml'
+                list_of_objects = load_object(object_dir + object_file_name, object_dir)
+                new_master_dict['ACL']['rules'][counter]['proto'] = list_of_objects
+
+            counter += 1
+        return new_master_dict
 
 
-def process_objects(raw_acl, object_dir):
-    rules = raw_acl['ACL']
-    for rule in rules['rules']:
-        try:
-            if ipaddress.ip_network(rule['source']):
-                print(rule['source'])
-        except ValueError as VE:
-            acl_object = (rule['source'].strip('{{ ').strip(' }}'))
-            print(load_file(object_dir + acl_object + '.yaml'))
+def load_object(file, object_dir):
+    with open(file, 'r') as stream:
+        list_of_objects = yaml.load(stream)
+        ''' list_of_objects can either be another reference, then this function will cal itself again, if not
+         then we return the actual list'''
 
-    return None
+        network_objects = []
+
+        for network_object in list_of_objects['object']:
+            if network_object[0:2] == '{{':
+                object_file_name = network_object.strip('{{').strip('}}') + '.yaml'
+                temp_objects = load_object(object_file_name, object_dir)
+                network_objects.append(temp_objects)
+            else:
+                network_objects.append(network_object)
+
+        return network_objects
 
 
 if __name__ == '__main__':
